29a5d1dc5eb4212db61bc60953a63ba6544733e2
Cheol J. Park <pffiro@gmail.com>
2022-01-06 15:54:54 +0900

    update lecture06

---
+++ numpy/lecture6.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+a = np.random.randint(1, 5, (5, ))
+b = np.random.randint(1, 5, (5, ))
+
+print("a: ", a)
+print("b: ", b)
+
+print("a + b: ", a + b)
+print("a - b: ", a - b)
+print("a * b: ", a * b)
+print("a / b: ", a / b)
+print("a // b: ", a // b)
+print("a % b: ", a % b)
+print("a ** b: ", a ** b)
+
+print("a > b: ", a > b)
+print("a >= b: ", a >= b)
+print("a < b: ", a < b)
+print("a <= b: ", a <= b)
+print("a == b: ", a == b)
+print("a != b: ", a != b)
+
+# Remember!
+# the shape of array should be same!
