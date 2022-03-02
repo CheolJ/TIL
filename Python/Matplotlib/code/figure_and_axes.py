4fd72f6c04d1610cc327ce591509c3ef625a7e35
CJ <pffiro@gmail.com>
2022-03-02 16:56:47 +0900

    figure and axes 예제 업데이트

---
+++ Python/Matplotlib/code/figure_and_axes.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(7, 7), facecolor='linen')
+ax1 = fig.add_subplot(2,3,1)
+ax2 = fig.add_subplot(2,3,2)
+ax3 = fig.add_subplot(2,3,4)
+ax4 = fig.add_subplot(2,3,5)
+ax_ir = fig.add_subplot(1,3,3)
+
+plt.show()
