13674b939379e3610ea1f3f4342c4f1dfe0e63e9
CJ <pffiro@gmail.com>
2022-03-04 17:46:37 +0900

    axescustomize 업데이트

--- Python/Matplotlib/code/axescustomize.py
+++ Python/Matplotlib/code/axescustomize.py
@@ -7,9 +7,9 @@ ax.set_ylabel('Y label', fontsize=15)
 
 plt.show()
 
-title_list = ['Ax' + str(i) i in range(4)]
-xlabel_list = ['X label ' + str(i) in range(4) ]
-ylabel_list = ['Y label ' + str(i) in range(4) ]
+title_list = ['Ax' + str(i) for i in range(4)]
+xlabel_list = ['X label ' + str(i) for i in range(4) ]
+ylabel_list = ['Y label ' + str(i) for i in range(4) ]
 
 fig, axes = plt.subplots(2, 2, figsize=(10, 10))
 for ax_idx, ax in enumerate(axes.flat):
@@ -17,8 +17,9 @@ for ax_idx, ax in enumerate(axes.flat):
     ax.set_xlabel(xlabel_list[ax_idx], fontsize=20)
     ax.set_ylabel(ylabel_list[ax_idx], fontsize=20)
 
-fig.tight_layout()
 
-fig.subplot_adjust()
+#fig.tight_layout()
+
+#fig.subplot_adjust()
 
 plt.show()
