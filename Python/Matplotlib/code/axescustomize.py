81487798872cf4d193fc8f435f7b91bbaf95b623
CJ <pffiro@gmail.com>
2022-03-04 17:44:11 +0900

    axescustomize 업데이트

---
+++ Python/Matplotlib/code/axescustomize.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize=(7, 7))
+ax.set_title('Title!', fontsize=20)
+ax.set_xlabel('X label', fontsize=15)
+ax.set_ylabel('Y label', fontsize=15)
+
+plt.show()
+
+title_list = ['Ax' + str(i) i in range(4)]
+xlabel_list = ['X label ' + str(i) in range(4) ]
+ylabel_list = ['Y label ' + str(i) in range(4) ]
+
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+for ax_idx, ax in enumerate(axes.flat):
+    ax.set_title(title_list[ax_idx], fontsize=30)
+    ax.set_xlabel(xlabel_list[ax_idx], fontsize=20)
+    ax.set_ylabel(ylabel_list[ax_idx], fontsize=20)
+
+fig.tight_layout()
+
+fig.subplot_adjust()
+
+plt.show()
---
+++ Python/Matplotlib/wiki/axescustomize.md
@@ -0,0 +1,25 @@
+# Axes customizing
+## `matplotlib.axes.Axes.set_title`
+`ax`에 제목을 
+### Doc
+```python
+matplotlib.axes.Axes.set_title(label, fontdict=None, loc=None, pad=None, **kwargs)
+```
+`dec commnet`  
+Set one of the three available Axes titles. The available titles are positioned above the Axes in the center, flush with the left edge, and flush with the right edge.
+
+## `matplotlib.axes.Axes.set_xlabel`
+### Doc
+```python
+```
+
+## `matplotlib.axes.Axes.set_ylabel`
+### Doc
+```python
+```
+
+## `matplotlib.figure.tight_layout`
+### Doc
+```python
+tight_layout(*, pad=1.08, h_pad=None, w_pad=None, rect=None)
+```
