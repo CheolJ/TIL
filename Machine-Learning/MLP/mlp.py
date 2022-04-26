55f15db1bee7004340e2b9e8b025d9ed90d14bfc
Cheolj <pffiro@gmail.com>
2022-04-26 15:02:15 +0900

    딥러닝 알고리즘 업데이트

---
+++ Machine-Learning/MLP/mlp.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y1 = sigmoid(x+0.5)
+y2 = sigmoid(x+1)
+y3 = sigmoid(x+1.5)
+
+plt.plot(x, y1, 'r', linestyle='--')
+plt.plot(x, y2, 'g')
+plt.plot(x, y3, 'b', linestyle='--')
+plt.plot([0,0], [1.0, 0.0], ':')
+plt.title('Sigmoid Function')
+plt.show()
